8f758bf86899ae5bd9fcfe25021bb20a84a7b1d3
Alex H <hooper.30@wright.edu>
2023-12-13 15:53:52 -0500

    Windows fix

---
+++ START.bat
@@ -0,0 +1,6 @@
+cd frontend
+call npm install
+start cmd /k ng serve
+start http://localhost:4200/ 
+cd ..
+call java -jar backend.jar 
--- START.sh
+++ START.sh
@@ -2,6 +2,5 @@ cd frontend
 npm install 
 ng serve &
 open http://localhost:4200/
-start http://localhost:4200/ 
 cd ..
 java -jar backend.jar  #Not async so this way ctrl c will actually work.
